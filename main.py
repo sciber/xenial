51b15adee29a55be50abcc728c664d11db498234
sciber <admin@sciber.net>
2019-10-08 00:10:45 +0100

    Group related UI components into separate .kv files

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import kivy
 
 from kivy.app import App
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import Screen
 from kivy.garden.navigationdrawer import NavigationDrawer
 
+Builder.load_file('ui-components.kv')
+Builder.load_file('search.kv')
+Builder.load_file('categories.kv')
+Builder.load_file('category.kv')
+Builder.load_file('tags.kv')
+Builder.load_file('tag.kv')
+Builder.load_file('articles.kv')
+Builder.load_file('article.kv')
+Builder.load_file('bookmarks.kv')
+Builder.load_file('settings.kv')
+Builder.load_file('guides.kv')
 
 kivy.require('1.11.1')
+
+
+class ArticleScreen(Screen):
+    def toggle_bookmark(self):
+        print('Article (un)bookmarked!')
 
 
 class ApplicationRoot(NavigationDrawer):
